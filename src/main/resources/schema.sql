d4129b3bf4d2462fa8f69df97f1d3826aad595f1
Mateusz <mateusz.skrzypczyk1@hotmail.com>
2021-04-26 11:25:25 +0200

    Formatted schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS statistics;
 
-CREATE TABLE statistics (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    login varchar(255) NOT NULL UNIQUE,
-    request_count INT8 NOT NULL
+CREATE TABLE statistics
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    login         varchar(255) NOT NULL UNIQUE,
+    request_count INT8         NOT NULL
 );
